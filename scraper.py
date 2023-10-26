07438a8a68ef4d920d849258366a98071d046bdc
Sven <108429493+svenfydrich@users.noreply.github.com>
2023-10-26 20:15:24 +0200

    output in csv und excel, details sortiert

--- scraper.py
+++ scraper.py
@@ -2,30 +2,39 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
 
-#Hier URL zum Scrapen eingeben
+# hier wird dir URL zum Scrapen eingegeben
 url = "https://www.muenchen.de/veranstaltungen/event/heute"
 driver = webdriver.Chrome()
 driver.get(url)
 
-#Hier CSS-Tags zum scrapen eingeben
-concerts = driver.find_elements(By.CSS_SELECTOR, "h3.m-event-list-item__headline")
+# hier werden die zu scrapende CSS-Tags eingegeben
+events = driver.find_elements(By.CSS_SELECTOR, "h3.m-event-list-item__headline") #das sind hier die namen der veranstaltungen
 dates_and_times = driver.find_elements(By.CSS_SELECTOR, "p time")
 locations = driver.find_elements(By.CSS_SELECTOR, '[itemprop="location"]')
 
+concert_data = {
+    "Name der Veranstaltung": [],
+    "Datum und Uhrzeit": [],
+    "Ort": []
+}
 
-concert_data = {}
-
-for concert, date_and_time, location in zip(concerts, dates_and_times, locations):
+for concert, date_and_time, location in zip(events, dates_and_times, locations):
     concert_name = concert.text
     date_and_time_text = date_and_time.text
     location_text = location.text
-    concert_data[concert_name] = date_and_time_text , location_text
-#Konzert-Daten zum Terminal printen
-for concert_name, date_and_time_text in concert_data.items():
-    print("Veranstaltung:", concert_name)
-    print("Datum und Uhrzeit:", date_and_time_text)
-    print("Location:", location_text)
-    print()
+    concert_data["Name der Veranstaltung"].append(concert_name)
+    concert_data["Datum und Uhrzeit"].append(date_and_time_text)
+    concert_data["Ort"].append(location_text)
 
-#Schließt 
+# schließt den web-driver
 driver.quit()
+
+# Create a DataFrame
+df = pd.DataFrame(concert_data)
+
+# exportiert den pandas dataframe zu einer excel-tabelle
+excel_file = "events_table.xlsx"
+df.to_excel(excel_file, index=False)
+
+#exportiert den pandas dataframe zu einer csv-datei
+df.to_csv("events_table.csv", index=False, sep='\t')
