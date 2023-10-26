285e1c4000797c4e494ea1ec79e5740803b10d1f
Sven <108429493+svenfydrich@users.noreply.github.com>
2023-10-26 15:41:37 +0200

    Scraped jetzt auch Ort, Datum und  Uhrzeit

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ url = "https://www.muenchen.de/veranstaltungen/event/heute" #hier URL der Websit
 driver = webdriver.Chrome() #öffnet die URL in Google Chrome
 driver.get(url)
 
-items = driver.find_elements(By.CSS_SELECTOR, "h3.m-event-list-item__headline") #Der String selektiert die CSS-tags
-
+#items = driver.find_elements(By.CSS_SELECTOR, "h3.m-event-list-item__headline") #Der String selektiert die CSS-tags. Hier: Titel des Events
+items = driver.find_elements(By.CSS_SELECTOR, "p.m-event-list-item__detail") # Ort, Datum und Uhrzeit
 for item in items:
     print(item.text)
