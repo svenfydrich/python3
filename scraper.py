4c794bda5346597427912e15e9bbd41f2cf8b65c
Sven <108429493+svenfydrich@users.noreply.github.com>
2023-10-26 19:50:11 +0200

    alle details sortiert, kein output in csv aktuell

--- scraper.py
+++ scraper.py
@@ -1,13 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-#importiert selenium, eine Library um CSS-tags zu scrapen
+import pandas as pd
 
-url = "https://www.muenchen.de/veranstaltungen/event/heute" #hier URL der Website zum scrapen eingeben
-
-driver = webdriver.Chrome() #öffnet die URL in Google Chrome
+#Hier URL zum Scrapen eingeben
+url = "https://www.muenchen.de/veranstaltungen/event/heute"
+driver = webdriver.Chrome()
 driver.get(url)
 
-#items = driver.find_elements(By.CSS_SELECTOR, "h3.m-event-list-item__headline") #Der String selektiert die CSS-tags. Hier: Titel des Events
-items = driver.find_elements(By.CSS_SELECTOR, "p.m-event-list-item__detail") # Ort, Datum und Uhrzeit
-for item in items:
-    print(item.text)
+#Hier CSS-Tags zum scrapen eingeben
+concerts = driver.find_elements(By.CSS_SELECTOR, "h3.m-event-list-item__headline")
+dates_and_times = driver.find_elements(By.CSS_SELECTOR, "p time")
+locations = driver.find_elements(By.CSS_SELECTOR, '[itemprop="location"]')
+
+
+concert_data = {}
+
+for concert, date_and_time, location in zip(concerts, dates_and_times, locations):
+    concert_name = concert.text
+    date_and_time_text = date_and_time.text
+    location_text = location.text
+    concert_data[concert_name] = date_and_time_text , location_text
+#Konzert-Daten zum Terminal printen
+for concert_name, date_and_time_text in concert_data.items():
+    print("Veranstaltung:", concert_name)
+    print("Datum und Uhrzeit:", date_and_time_text)
+    print("Location:", location_text)
+    print()
+
+#Schließt 
+driver.quit()
