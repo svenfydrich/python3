6a954a7212b2b0e5d803e4c037fbac381ec46e66
Sven <108429493+svenfydrich@users.noreply.github.com>
2023-10-26 15:35:55 +0200

    Kommentare hinzugefügt

--- scraper.py
+++ scraper.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+#importiert selenium, eine Library um CSS-tags zu scrapen
 
-url = "https://www.muenchen.de/veranstaltungen/event/heute"
+url = "https://www.muenchen.de/veranstaltungen/event/heute" #hier URL der Website zum scrapen eingeben
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome() #öffnet die URL in Google Chrome
 driver.get(url)
 
-items = driver.find_elements(By.CSS_SELECTOR, "h3.m-event-list-item__headline")
+items = driver.find_elements(By.CSS_SELECTOR, "h3.m-event-list-item__headline") #Der String selektiert die CSS-tags
 
 for item in items:
     print(item.text)
